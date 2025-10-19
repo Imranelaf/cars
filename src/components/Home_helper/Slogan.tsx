110a51d28feefa09be3d26ad4729b133816fd75c
Imrane ALI LAFKIH <imr07ane@gmail.com>
2025-10-19 23:33:49 +0100

    fix(Slogan): disable image drag and correct Pixar text loop

--- src/components/Home_helper/Slogan.tsx
+++ src/components/Home_helper/Slogan.tsx
@@ -13,6 +13,7 @@ export default function Slogan(){
                 src="./images/mcqueen.gif" 
                 alt="mcqueen image" 
                 className="w-50 select-none mt-2"
+                draggable={false}
                 />
             <h1>Trip</h1>
             </div>
@@ -20,11 +21,8 @@ export default function Slogan(){
             <div className="relative h-full">
                 <div className="border-2 border-[#eae7d4] w-auto h-[85vh] flex flex-col gap-9 rounded-sm mt-2 overflow-hidden">
                     <div className="scrollingBar flex flex-col gap-9">
-                        {Array.from({ length: 8}).map((_, index) => (
+                        {Array.from({ length: 16}).map((_, index) => (
                             <p key={index} className="text-[#eae7d4] rotate-270 mt-4 tracking-wider font-semibold">PIXAR</p>
-                        ))}
-                        {Array.from({ length: 8 }).map((_, index) => (
-                            <p key={`duplicate-${index}`} className="text-[#eae7d4] rotate-270 mt-4 tracking-wider font-semibold">PIXAR</p>
                         ))}
                     </div>
                 </div>
