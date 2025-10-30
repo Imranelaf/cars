57d5e7193dd03975be0f29c7ce100347a34a494c
Imrane ALI LAFKIH <imr07ane@gmail.com>
2025-10-30 11:27:59 +0000

    separated video cards animation into its own file and improved responsive design

---
+++ src/components/Animations/videosCardsAnimations.ts
@@ -0,0 +1,40 @@
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+
+export default function useVideoCardsAnimation() {
+  useGSAP(() => {
+    gsap.registerPlugin(ScrollTrigger)
+
+    const cards = gsap.utils.toArray(".videos")
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".videoContainer",
+        pin: true,
+        scrub: 1,
+        start: "top top",
+        end: `+=${window.innerHeight * (cards.length - 1)}`
+      }
+    })
+
+    // Set initial states for the last cards only
+    gsap.set(cards.slice(2), { rotate: 15, scale: 0.5, opacity:0})
+
+    cards.forEach((video, i) => {
+      if (i > 1) {
+        tl.to(video, {
+          rotate: 0,
+          scale: 1,
+         opacity:1,
+          duration: .2,
+          
+        }, '<')
+      }
+
+      if (i !== cards.length - 1) {
+        tl.to(cards, { yPercent: -95 * (i + 1), duration: 1, ease: "none" })
+      }
+    })
+  }, [])
+}
--- src/components/Home_helper/VideosCards.tsx
+++ src/components/Home_helper/VideosCards.tsx
@@ -1,9 +1,10 @@
-import { useRef } from "react"
-import { useGSAP } from "@gsap/react"
-import gsap from "gsap"
-import { ScrollTrigger } from "gsap/ScrollTrigger"
-import Infos from "./Informations"
+import { useRef } from "react";
+import Infos from "./Informations";
 
+import useVideoCardsAnimation from "../Animations/videosCards";
+
+
+/* the data for each video */
 const data = [
     {
         src: '1.mp4',
@@ -29,48 +30,41 @@ const data = [
 
 export default function VideosCards() {
 
+    /* make a reference to each video in array to target it */ 
     const videoRef = useRef([])
+  
    
 
-    const playVideo = (index)=>{
+    /* Playing video by hovering effect */
+    const playVideo = (index:number)=>{
         videoRef.current && videoRef.current[index].play()
     }
 
-    const pausedVideo = (index)=>{
+    const pausedVideo = (index:Number)=>{
+        console.log('This is the index', index);
+        console.log('This is the current', videoRef.current);
+        
         if(videoRef.current){ 
             videoRef.current[index].pause()
             videoRef.current[index].currentTime = 0 
         }}
 
 
-    useGSAP(()=>{
-        gsap.registerPlugin(ScrollTrigger)
-        const videosNum = gsap.utils.toArray('.videos');
+    
+    /*Animation the cards by scrolling vertical and fade in to screen in animation folder */
+    useVideoCardsAnimation()
 
-        gsap.to(videosNum,{
-            yPercent: -90 * (videosNum.length - 1),
-            ease: 'none',
-            scrollTrigger:{
-                trigger: '.videoContainer',
-               pin:true,
-                scrub: 1,
-                start:'top top',
-                end:`+=${window.innerHeight * (videosNum.length - 1)}`,
-               
-            }
-        })
-    }, [])
 
 
    return(
-        <section className="w-screen lg:flex videoContainer">
-            <div className="relative lg:w-1/2 w-full h-screen videoTrigger flex flex-col gap-5 overflow-hidden">
+        <section className="w-screen lg:h-screen h-[200vh] lg:flex videoContainer">
+            <div className="relative lg:w-1/2 p-1 w-full h-screen videoTrigger flex flex-col gap-5 overflow-hidden">
 
            {
             data.map((item,index)=>(
 
                 <div key={item.src}
-                className="w-full h-screen videos rounded-sm bg-[#eae7d4] text-red-500 p-1 lg:p-4 text-center cursor-pointer hover:bg-red-500 hover:text-[#eae7d4] duration-500 flex flex-col justify-center items-center"
+                className="w-full h-min videos rounded-sm bg-[#eae7d4] text-red-500 p-1 py-2 lg:p-4 text-center cursor-pointer hover:bg-red-500 hover:text-[#eae7d4] duration-500 flex flex-col justify-center items-center"
                 onMouseEnter={()=>playVideo(index)}
                 onMouseLeave={()=>pausedVideo(index)}
                 >
