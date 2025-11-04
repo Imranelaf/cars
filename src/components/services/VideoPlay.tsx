037b0fba20c33a02fb2ace419608ca363a4597cf
Imrane ALI LAFKIH <imr07ane@gmail.com>
2025-11-04 16:36:14 +0000

    fix(VideoPlay): make video responsive during playback

--- src/components/services/VideoPlay.tsx
+++ src/components/services/VideoPlay.tsx
@@ -24,12 +24,12 @@ export default function VideoPlay({ link, setVideo }: VideoLink) {
     <div className="absolute inset-0 h-screen flex justify-center items-center bg-gray-900 bg-opacity-70 backdrop-blur-sm">
       <button
         onClick={close}
-        className="p-3 rounded-full bg-red-500 absolute top-10 right-20 z-10 cursor-pointer hover:rotate-180 duration-300"
+        className="p-3 rounded-full bg-red-500 absolute top-10 lg:right-20 z-10 cursor-pointer hover:rotate-180 duration-300"
       >
         <img src="images/icon-close.svg" alt="Close icon" />
       </button>
 
-      <video src={`./videos/${link}`} className="w-8/12 h-8/12 object-cover rounded-lg" controls autoPlay />
+      <video src={`./videos/${link}`} className="lg:w-8/12 lg:h-8/12 w-full h-3/5 p-1 object-cover rounded-lg" controls autoPlay />
     </div>
   );
 }
