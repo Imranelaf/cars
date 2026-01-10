37c000149e767c125dbeb6e8938059c14ea4cc8c
Imrane ALI LAFKIH <imr07ane@gmail.com>
2026-01-10 23:05:06 +0000

    refactor: make AboutAnimation responsive with mobile breakpoints

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -5,80 +5,79 @@ import { useGSAP } from "@gsap/react"
 import Image from "next/image"
 import { useState } from "react"
 
-const data=[
+const data = [
     {
-        fname:'John ',
+        fname: 'John ',
         lname: 'Lasseter',
         job: 'Director',
         pic: '/images/Crew/John_Lasseter.webp',
         bio: "John Lasseter is a pivotal American animator, director, and producer, famous for spearheading Pixar Animation Studios and revolutionizing CGI films like Toy Story, Cars, and A Bug's Life, alongside his leadership roles at Walt Disney Animation Studios before departing in 2018 due to misconduct allegations. He began at Disney, was fired for pushing computer animation, then joined Lucasfilm's computer graphics division (which became Pixar), directing classics and serving as Chief Creative Officer at both studios, earning multiple Academy Awards. "
 
     },
-     {
-        fname:'Joe ',
-        lname:'Ranft',
+    {
+        fname: 'Joe ',
+        lname: 'Ranft',
         job: 'Co-director/Writer',
         pic: '/images/Crew/Joe_Ranft.webp',
         bio: "Joseph 'Joe' Ranft (1960-2005) was a celebrated American animator, screenwriter, storyboard artist, and voice actor, known for his pivotal creative roles at Pixar and Disney, contributing to classics like Toy Story, The Lion King, Beauty and the Beast, A Bug's Life, and Cars, voicing beloved characters like Heimlich and Jacques, and tragically passing away in a car accident at age 45, leaving a legacy as a 'Disney Legend'. "
 
     },
-     {
-        fname:'Owen ',
-        lname:'Wilson',
+    {
+        fname: 'Owen ',
+        lname: 'Wilson',
         job: "McQueen'S voice",
         pic: '/images/Crew/Owen_Wilson.webp',
         bio: "Owen Cunningham Wilson (born November 18, 1968)[3] is an American actor and screenwriter. He has frequently worked with filmmaker Wes Anderson, with whom he has shared writing and acting credits on the films Bottle Rocket (1996), Rushmore (1998), and The Royal Tenenbaums (2001)—the latter received a nomination for the Academy Award and BAFTA Award for Best Screenplay. He has also appeared in Anderson's The Life Aquatic with Steve Zissou (2004), The Darjeeling Limited (2007), Fantastic Mr. Fox (2009), The Grand Budapest Hotel (2014), and The French Dispatch (2021)."
     },
 ]
-    
+
 export type Person = typeof data[0]
 
 export default function About() {
 
-  const [activePerson, setActivePerson]= useState<Person | null>(null)
+    const [activePerson, setActivePerson] = useState<Person | null>(null)
 
-  
 
-        useGSAP(() => {
-          AboutAnimation()
-  },
-)
 
-        const cardClick = (item: typeof data[0]  )=>{
+    useGSAP(() => {
+        AboutAnimation()
+    },)
+
+    const cardClick = (item: typeof data[0]) => {
         document.body.style.overflow = "hidden"
         setActivePerson(item)
     }
 
-    
+
 
     return (
-       
+
         <section className="bgColor w-full min-h-screen h-fit overflow-x-hidden">
             <div className="w-full h-fit flex flex-col items-center p-4">
                 <h1 className="text-[#eae7d4] chunko lg:text-9xl text-6xl lg:p-4 p-2 text-center uppercase ">A World Built on <br />
-                    <span className="text-red-500 speed inline-block "> Speed </span> 
-                    and 
+                    <span className="text-red-500 speed inline-block "> Speed </span>
+                    and
                     <span className="text-red-500 heart inline-block"> Heart </span>
                 </h1>
                 <p className="lg:w-1/2 w-full lg:text-3xl text-lg Grand-Regular text-center tracking-wider italic m-4 text">
-                    Released in 2006 by Pixar Animation Studios and distributed by Walt Disney Pictures, 
-                    Cars takes place in a world where vehicles are alive, each with their own personality, 
-                    dreams, and flaws. At the center of the story is Lightning McQueen, a rising star who learns that 
-                    true victory isn’t just about crossing the finish line, but about friendship, humility, 
+                    Released in 2006 by Pixar Animation Studios and distributed by Walt Disney Pictures,
+                    Cars takes place in a world where vehicles are alive, each with their own personality,
+                    dreams, and flaws. At the center of the story is Lightning McQueen, a rising star who learns that
+                    true victory isn’t just about crossing the finish line, but about friendship, humility,
                     and finding meaning off the beaten path.
                 </p>
             </div>
-            
-      
- 
+
+
+
             <div className="min-h-screen h-fit w-full p-2 lg:flex gap-3 imagePoster">
 
                 {
-                    data.map((item)=>(
-                        <div key={item.lname} className="h-fit card group my-2 lg:w-1/3 w-full bg-[#eae7d4] hover:bg-[#ded6a6] duration-300 uppercase p-4 rounded-sm flex flex-col items-center text-center cursor-pointer" 
-                            onClick={()=>cardClick(item)
+                    data.map((item) => (
+                        <div key={item.lname} className="h-fit card group my-2 lg:w-1/3 w-full bg-[#eae7d4] hover:bg-[#ded6a6] duration-300 uppercase p-4 rounded-sm flex flex-col items-center text-center cursor-pointer"
+                            onClick={() => cardClick(item)
                             }
-                          >
+                        >
                             <Image
                                 src={item.pic}
                                 alt="Jeo Ranft"
@@ -86,29 +85,28 @@ export default function About() {
                                 width={450}
                                 className="relative h-1/2 w-full lg:h-3/5 lg:w-4/5 rounded-sm group-hover:scale-105 duration-500"
                             />
-                            
+
                             <h1 className="text-xl font-bold underline ">{item.job}</h1>
                             <div className="flex flex-row ">
-                            <h1 className="lg:text-7xl text-5xl flex flex-col chunko-bold text-red-500">{item.lname} <span className=" text-black ">{item.fname} </span></h1>
-                    </div>
-                    
+                                <h1 className="lg:text-7xl text-5xl flex flex-col chunko-bold text-red-500">{item.lname} <span className=" text-black ">{item.fname} </span></h1>
+                            </div>
 
-                </div>
+
+                        </div>
 
                     ))
                 }
 
 
-            
+
 
             </div>
 
-            { activePerson &&
-                <ShowCastsInfo activePerson={activePerson} setActivePerson={setActivePerson}/>
+            {activePerson &&
+                <ShowCastsInfo activePerson={activePerson} setActivePerson={setActivePerson} />
             }
 
-            
-   
+
 
         </section>
     )
--- components/animations/AboutAnimation.tsx
+++ components/animations/AboutAnimation.tsx
@@ -2,46 +2,49 @@ import gsap from "gsap/all"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
 import { SplitText } from "gsap/SplitText"
 
-export default function AboutAnimation(){
-     gsap.registerPlugin(SplitText, ScrollTrigger)
-    
-    
-          /* text animation */
-        const split = SplitText.create('.text', { type: 'words' })
-    
-        gsap.from('.speed', {
-          x: '+=120vw',
-          duration: 1,
-          ease: 'bounce.out',
-        })
-    
-        gsap.fromTo(
-          '.heart',
-          { scale: 0.9 },
-          {
-            scale: 1,
-            duration: 0.7,
-            repeat: -1,
-            repeatDelay: 0.5,
-            ease: 'elastic.out',
-          }
-        )
-    
-        document.fonts.ready.then(() => {
-          gsap.from(split.words, {
-            yPercent: 'random[-400,400]',
-            rotation: 'random[-30,30]',
-            autoAlpha: 0,
-            stagger: {
-              amount: 1,
-              from: 'random',
-            },
-          })
-        })
-    
-          /* Cards animation */
-    
-          const tl = gsap.timeline({
+export default function AboutAnimation() {
+  gsap.registerPlugin(SplitText, ScrollTrigger)
+  
+  const mm = gsap.matchMedia()
+  
+  /* Text animation */
+  const split = SplitText.create('.text', { type: 'words' })
+  
+  gsap.from('.speed', {
+    x: '+=120vw',
+    duration: 1,
+    ease: 'bounce.out',
+  })
+  
+  gsap.fromTo(
+    '.heart',
+    { scale: 0.9 },
+    {
+      scale: 1,
+      duration: 0.7,
+      repeat: -1,
+      repeatDelay: 0.5,
+      ease: 'elastic.out',
+    }
+  )
+  
+  document.fonts.ready.then(() => {
+    gsap.from(split.words, {
+      yPercent: 'random[-400,400]',
+      rotation: 'random[-30,30]',
+      autoAlpha: 0,
+      stagger: {
+        amount: 1,
+        from: 'random',
+      },
+    })
+  })
+  
+  /* Cards animation */
+  
+  // big screens
+  mm.add("(min-width: 800px)", () => {
+    const tl = gsap.timeline({
       scrollTrigger: {
         trigger: '.imagePoster',
         pin: true,
@@ -50,27 +53,50 @@ export default function AboutAnimation(){
         scrub: 2,
       },
     })
-        tl.fromTo(
-          '.card',
-          {
-            x: '100vw',
-            rotate: 50,
-            scale: 0.6,
-          },
-          {
-            x: 0,
-            rotate: 0,
-            ease: 'power3.out',
-            stagger: 0.15,
-          }
-        ).to('.card',{
-          scale:1,
-          ease:'power1.inOut',
-          stagger:.2
-        })
     
+    tl.fromTo(
+      '.card',
+      {
+        x: '100vw',
+        rotate: 50,
+        scale: 0.6,
+      },
+      {
+        x: 0,
+        rotate: 0,
+        ease: 'power3.out',
+        stagger: 0.15,
+      }
+    ).to('.card', {
+      scale: 1,
+      ease: 'power1.inOut',
+      stagger: 0.2
+    })
     
-        tl.to({}, { duration: 1.5 }) // extend pinned section to keep cards visible longer
+    tl.to({}, { duration: 1.5 }) // extend pinned section
+  })
+  
+  // small screens 'Mobile'
+  mm.add("(max-width: 799px)", () => {
+    const cards = gsap.utils.toArray<HTMLElement>('.card')
     
-    return null
+    cards.forEach((card) => {
+      gsap.from(card, {
+        opacity: 0,
+        y: 100,
+        rotate: 15,
+        scale: 0.8,
+        duration: 0.8,
+        ease: 'power2.out',
+        scrollTrigger: {
+          trigger: card,
+          start: 'top 80%',
+          end: 'top 80%',
+          toggleActions: 'play none none reverse',
+        }
+      })
+    })
+  })
+  
+  return null
 }
